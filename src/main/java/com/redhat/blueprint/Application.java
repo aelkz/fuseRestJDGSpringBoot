5b2069736db3453d494aa7d30e339688a8292915
raphael <raphael.alex@gmail.com>
2019-03-04 18:16:48 -0300

    Initial Release

--- src/main/java/com/redhat/blueprint/Application.java
+++ src/main/java/com/redhat/blueprint/Application.java
@@ -20,17 +20,33 @@ package com.redhat.blueprint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
     private static Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @Autowired
-    private Environment env;
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.url}")
+    private String oracleDatasourceUrl;
+
+    @Value("${OPENSHIFT_ORACLE_DATASOURCE_HOST}")
+    private String v1;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(jdbcUrl);
+        System.out.println(oracleDatasourceUrl);
+        System.out.println(v1);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/redhat/blueprint/route/BlueprintRouteBuilder.java
+++ src/main/java/com/redhat/blueprint/route/BlueprintRouteBuilder.java
@@ -33,8 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 import javax.ws.rs.core.MediaType;
 import java.net.ConnectException;
 import java.util.concurrent.TimeUnit;
